20731bed2447cad4a25e358d5c88a0993b386aa4
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-25 14:58:12 +0600

    some()

---
+++ some.js
@@ -0,0 +1,31 @@
+// Some() Definition
+// When we need to check that if the element exist or not, can in array and object as well and it'll return false or true
+
+
+
+// Array checking
+const array = ["Mark", 1, 21, 55, "Jon"]
+array.some(function (element){
+    console.log(element);
+    return element === 1
+})
+
+
+
+// Object checking
+const myObj = [
+    {
+        age: 23,
+        job: "Teacher"
+    },
+    {
+        age: 25,
+        job: "Employee"
+    },
+    {
+        age: 20,
+        job: "Banker"
+    }
+]
+myObj.some(ageProp => console.log(`${ageProp.age === 23} : ${ageProp.age}`))
+myObj.some(ageProp => console.log(ageProp.job === "Teacher"))
